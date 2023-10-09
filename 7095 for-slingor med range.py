61bbee4408b4861a881015f734fc65a25f24a963
RiadSantir <santirriad@gmail.com>
2023-10-09 13:22:21 +0200

    Nytt

---
+++ 7095 for-slingor med range.py
@@ -0,0 +1,20 @@
+'''
+for i in range(5):
+    print(i)
+'''
+'''
+for i in range(3, 8):
+    print(i)
+'''
+'''
+for i in range (4, 20, 3):
+    print(i)
+'''
+'''
+for i in range (20, 40, 5):
+    print(i)
+'''
+'''
+for i in range (11, 0, -1):
+    print(i)
+'''
---
+++ 7096 for each-slinga.py
@@ -0,0 +1,19 @@
+'''
+text = "1001011"
+
+antalettor = 0
+
+for siffra in text: 
+    if siffra == '1':
+        antalettor = antalettor + 1
+print(f"antal ettor: {antalettor}")
+'''
+
+text = "13324343532611465515256554532451154255241636666454"
+
+antalsexor = 0
+
+for siffra in text: 
+    if siffra == '6':
+        antalsexor = antalsexor + 1
+print(f"antal ettor: {antalsexor}")
