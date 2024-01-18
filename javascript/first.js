537cff7dfd9c11205c946c1b7ec9e6b37e192efb
Riadsantir <112951270+Riadsantir@users.noreply.github.com>
2024-01-18 13:29:58 +0100

    Datatyper i JS

--- javascript/first.js
+++ javascript/first.js
@@ -1,1 +1,21 @@
 document.querySelector("h1").style.color = "Blue";
+
+/* 
+Data types 
+String 
+Number 
+Array => Object 
+Object 
+Boolean
+*/
+
+console.log(typeof "Riad Santir");
+console.log(typeof 5000);
+console.log(typeof 5000.99);
+console.log(typeof [10, 15, 16]);
+console.log(typeof ["Riad", "Santir", "Nösnäs"]);
+console.log(typeof { name: "Riad", age: 19, country: "Sweden"});
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
